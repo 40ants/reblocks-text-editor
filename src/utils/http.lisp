86ee8b8c8782e133b36310e46ccf0e66dd68f915
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-03-06 13:49:26 +0300

    Fixed a function for getting site's title in case of any errors.

--- src/utils/http.lisp
+++ src/utils/http.lisp
@@ -8,12 +8,15 @@
 
 
 (defun retrieve-url-title (url)
-  (let* ((data (dex:get url
-                        :connect-timeout 1
-                        :read-timeout 1))
-         (tree (plump:parse data))
-         (titles (clss:select "title" tree))
-         (title (when (length>= titles 1)
-                  (plump:text
-                   (elt titles 0)))))
-    title))
+  (handler-case
+      (let* ((data (dex:get url
+                            :connect-timeout 1
+                            :read-timeout 1))
+             (tree (plump:parse data))
+             (titles (clss:select "title" tree))
+             (title (when (length>= titles 1)
+                      (plump:text
+                       (elt titles 0)))))
+        title)
+    (error ()
+      nil)))
